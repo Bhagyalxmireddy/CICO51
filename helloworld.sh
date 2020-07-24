167e08ae298c70d47f89375ab01d4f9d880737fe
Bhagi <puppyreddy1997@gmail.com>
2020-07-24 11:42:32 +0530

    My first file upload

---
+++ helloworld.sh
@@ -0,0 +1,14 @@
+public class Helloworld
+{
+     public static void main(String[] args)
+   {
+        System.out.println("hello to terminal command");
+         while(true)
+           {
+            try{
+            Thread.sleep(6000);
+System.out.println("hello to teminal command");
+             }catch(Exception e){}
+            }
+     }
+}
